92dde0f219eaebe8d91c804ff35ac28a47593991
The Yaegerist <awoonor1z@gmail.com>
2024-06-10 08:21:48 +0000

    more exercises

---
+++ chapter 1-4/animals.py
@@ -0,0 +1,4 @@
+animals =['eagle', 'sparrow', 'blue jay', 'mockingjay']
+for animal in animals:
+  print(f'a {animal} would make a great pet')
+print('\nthey are all birds')
---
+++ chapter 1-4/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek','redline','specialized']
+print (bicycles[1].upper())
+
+#negative index lists call from the end of the list
+print (bicycles[-1].title())
+
+message = f"my first ever bicycle was a {bicycles[0].title()} type of bicycle"
+print (message)
