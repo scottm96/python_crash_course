57eeb49efb0343d3383d3ed4c8f98279ed8990d1
The Yaegerist <awoonor1z@gmail.com>
2024-06-12 17:48:47 +0000

    full name exercise

---
+++ chapter 1-4/full_name.py
@@ -0,0 +1,12 @@
+first_name = "max"
+last_name = "scott"
+full_name = f"{first_name} {last_name}"
+message = f"welcome to valhalla, {full_name.title()}"
+print (message.title())
+message =("over here you will learn the following python jargs:\n\tdjango\n\talien shooter game\n\tand some other projects")
+print (message.title())
+message = ("when you are done you are going to realize you are now one of the best python \nprogrammers \t developpers")
+print(message)
+fav_lang = " pyhton py"
+print (fav_lang)
+print(fav_lang.rstrip())
