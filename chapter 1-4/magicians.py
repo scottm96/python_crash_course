254ff969736c219b02be4274167235be6eeff7b1
The Yaegerist <awoonor1z@gmail.com>
2024-06-13 10:03:13 +0000

    a pyhton exercise

---
+++ chapter 1-4/magicians.py
@@ -0,0 +1,5 @@
+magicians=['wale', 'quaz','yemz']
+for magician in magicians:
+    print(f"Wow {magician.title()}, that was a great performance")
+    print (f"i can't wait to see your next trick, {magician.title()}\n")
+print('\nThank you all for a great performance') 
