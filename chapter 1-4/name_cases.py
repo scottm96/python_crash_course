b48dbf10e0992ba3c1acd8fe48bd2d61970b59c4
The Yaegerist <awoonor1z@gmail.com>
2024-06-15 23:26:15 +0000

    tis the season

---
+++ chapter 1-4/name_cases.py
@@ -0,0 +1,6 @@
+first_name = "dennis"
+last_name = "agbanyo"
+full_name = f"{last_name}, {first_name}"
+print (full_name.lower())
+print (full_name.title())
+print (full_name.upper())
---
+++ chapter 1-4/number_eight.py
@@ -0,0 +1,9 @@
+
+#printing some equations and my favorite number
+print(4+4)
+print(2*4)
+print(10-2)
+print(24/3)
+
+fav_num = 9
+print(f"my favorite nummber is {fav_num}")
