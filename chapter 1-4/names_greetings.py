6e2aeccf8d9a6abe7123850199ebd92b7fefa230
The Yaegerist <awoonor1z@gmail.com>
2024-06-15 23:27:33 +0000

    some more

---
+++ chapter 1-4/names_greetings.py
@@ -0,0 +1,11 @@
+names = ['melanie','carrie', 'lockhart' , 'kenny' , 'jokavic']
+greeting = f"hello {names[0]}"
+print (greeting)
+greeting = f"hello {names[1]}"
+print (greeting)
+greeting = f"hello {names[2]}"
+print (greeting)
+greeting = f"hello {names[3]}"
+print (greeting)
+greeting = f"hello {names[4]}"
+print (greeting)
