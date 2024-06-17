327d02d9b9f8290484379be5a6830620eafbe1b9
The Yaegerist <awoonor1z@gmail.com>
2024-06-17 09:33:56 +0000

    orgs

---
+++ chapter 1-4/organizing.py
@@ -0,0 +1,17 @@
+cars = ['bmw', 'jaguar', 'vitz', 'kia']
+print(cars)
+
+#sorting the list in alphabetical order
+cars.sort()
+print(cars)
+
+#reverse alphabetical order
+cars.sort(reverse=True)
+print(cars)
+cars.sort()
+
+#sorting for only an instance
+print (sorted(cars))
+
+#length of the list
+print(len(cars))
