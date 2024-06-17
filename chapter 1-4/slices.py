eb82919eac6b17dc051f03dafb4f23fec8d6b778
The Yaegerist <awoonor1z@gmail.com>
2024-06-17 09:37:53 +0000

    how to slice lists

---
+++ chapter 1-4/slices.py
@@ -0,0 +1,5 @@
+players = ['charles','martina','michael','florence','eli'] 
+print(f'\n\nthe first three players on my list are: {players[:2]}')
+print(f'\n\nthree players from the middle of the list are: {players[1:4]}')
+print(f'\n\nthe last three players on my list are: {players[-3:]}')
+
