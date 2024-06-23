c87a7c42c71eb87aaadbe11b7b24ea5617fb4fb2
The Yaegerist <awoonor1z@gmail.com>
2024-06-23 12:24:02 +0000

    some dictionaries

---
+++ chapter_5/alien_colors2.py
@@ -0,0 +1,14 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you just earned 5 points')
+else:
+    print('you just earned 10 points')
+
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('you just earned 5 points')
+else:
+    print('you just earned 10 points')
