a7580ff203ebac9177fa6e5e32bef2703273855a
The Yaegerist <awoonor1z@gmail.com>
2024-06-26 07:18:52 +0000

    more dictionaries

---
+++ chapter_5/alien_colors3.py
@@ -0,0 +1,10 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you just earned 5 points')
+elif alien_color == 'yellow':
+    print('you just earned 10 points')
+elif alien_color == 'red':
+    print('you just earned 15 points')
+else:
+    print('invalid')
