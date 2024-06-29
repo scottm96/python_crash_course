cdbc2ff4b22bf2c35a56ac6611ea7162a08ffc81
The Yaegerist <awoonor1z@gmail.com>
2024-06-29 09:52:55 +0000

    testing conditions on numbers

---
+++ chapter_5/ordinal_numbers.py
@@ -0,0 +1,12 @@
+ord_numbers =[1,2,3,4,5,6,7,8,9]
+
+for num in ord_numbers:
+    if num == 1:
+        print(f'{num}st')
+    elif num == 2:
+        print(f'{num}nd')
+    elif num == 3:
+        print(f'{num}rd')
+    else:
+        print(f'{num}th')
+
