87c044cfaac74d42aeb2a95b627a5c908afa5963
The Yaegerist <awoonor1z@gmail.com>
2024-06-29 09:51:56 +0000

    mmore examples

---
+++ chapter_5/conditional_tests.py
@@ -0,0 +1,16 @@
+car ="audi"
+print("Is car == 'subaru'? i predict True")
+print(car=='subaru')
+
+print("Is car == 'audi'? i predict False")
+print(car=='audi')
+
+if car == 'audi':
+    print('you guessed right')
+elif car == 'toyota':
+    print('toyota cat lol but no')
+else:
+    print('you are wrong please try again')
+
+water = 'chicken'
+print(water.title() == 'Chicken')
